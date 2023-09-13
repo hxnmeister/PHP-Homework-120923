0feafcb8a8af78f468b91247412999d282574b52
Valerii <valeriy.stronskiy@gmail.com>
2023-09-13 14:57:10 +0300

    Task 1 done

---
+++ task-2/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css">
+            <title>Text Formater</title>
+        </head>
+        <body>
+            <div>
+                <form action="./result.php" method="post">
+                    <label for="text-to-format">Text:</label>
+                    <textarea name="textfield" id="text-to-format" cols="30" rows="7" required></textarea>
+                    <br>
+
+                    <label for="font">Font Family:</label>
+                    <select name="fontpicker" id="font-family">
+                        <option value="Arial, Helvetica, sans-serif">Arial</option>
+                        <option value="Verdana, Geneva, Tahoma, sans-serif">Verdana</option>
+                        <option value="Georgia, Times New Roman, Times, serif">Georgia</option>
+                        <option value="Courier New, Courier, monospace">Courier</option>
+                        <option value="Times New Roman, Times, serif">Times New Roman</option>
+                    </select>
+                    <br>
+                    
+                    <label for="size">Font Size:</label>
+                    <input type="number" name="fontsize" step="1" min="10" max="25" value="10" id="size">
+                    <br>
+
+                    <input type="checkbox" name="fontstyle[]" value="bold" id="style-bold">
+                    <label for="style-bold"><b>Bold</b></label>
+                    <br>
+                    <input type="checkbox" name="fontstyle[]" value="italic" id="style-italic">
+                    <label for="style-italic"><i>Italic</i></label>
+                    <br>
+                    <input type="checkbox" name="fontstyle[]" value="underline" id="style-underline">
+                    <label for="style-underline"><u>Underline</u></label>
+                    <br>
+
+                    <label for="color">Color:</label>
+                    <input type="text" name="fontcolor" value="Black" id="color">
+                    <br>
+
+                    <input type="submit" value="Format">
+                </form>
+            </div>
+        </body>
+    </html>
---
+++ task-2/result.php
@@ -0,0 +1,30 @@
+<?php
+    //Розмір шрифта
+    $fontSize = $_POST['fontsize'];
+    //Колір шрифта з очисткою пробілів, конвертацією в рядок тегів та переведенням у нижній регістр
+    $fontColor = strtolower(trim(htmlentities($_POST['fontcolor'])));
+    //Стилі тексту
+    $fontStyle = $_POST['fontstyle'] ?? [];
+    //Саме шрифт який буде застосовано
+    $fontFamily = $_POST['fontpicker'];
+    //Текст від користувача, додатково використав функцію "nl2br()" для збереження переносу на новий рядок
+    $receivedText = nl2br(trim(htmlentities($_POST['textfield'])));
+    //Маисв кольорів для перевірки правильності вводу кольору від користувача
+    $colors = ['red','blue','green','black','white','yellow','orange','pink','gray','purple',
+                'brown','lightblue','beige','lime','olive','maroon','lavender','pomegranate','lightpink','slategray'];
+                
+    if(!empty($receivedText)):
+        //Змінна $formatedText дозволить додати стилі до тексту за допомогою конкатенації рядків нижче
+        $formatedText = "<p style='font-family: {$fontFamily}; font-size: {$fontSize}; color: ".(in_array($fontColor, $colors) ? $fontColor : 'black')."; ";
+        
+        if(!empty($fontStyle)):
+            foreach($fontStyle as $style):
+                if($style === 'bold') $formatedText .= "font-weight: bold; ";
+                elseif($style === 'italic') $formatedText .= "font-style: italic; ";
+                elseif($style === 'underline') $formatedText .= "text-decoration: underline; ";
+            endforeach;
+        endif;
+        
+        $formatedText .= "'>{$receivedText}</p>";
+        echo $formatedText;
+    endif;
